41dd48d888638bedae1496aa83694744323a7c85
ayrat <kaz90@bk.ru>
2019-02-15 22:27:02 +0300

    Added Doctest

--- lib.py
+++ lib.py
@@ -1,4 +1,8 @@
 def dist_calc(expense_fuel, volume_fuel):
+    """
+    >>> dist_calc(10,20)
+    180
+    """
     stock = 20 #20 км для запаса
     dist_calc = int((volume_fuel *  100 / expense_fuel) - stock)
     return dist_calc
